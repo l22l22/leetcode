93838ed716bb56710a3d92f82d49bef1701d5055
gowoon <101627078+l22l22@users.noreply.github.com>
2022-04-04 13:52:48 +0900

    Update Best_Time_to_Buy_and_Sell_Stock_II.java

--- array/Best_Time_to_Buy_and_Sell_Stock_II.java
+++ array/Best_Time_to_Buy_and_Sell_Stock_II.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public int maxProfit(int[] prices) {
+        // 이익
+        int profit = 0;
+        for(int i = 0 ; i < prices.length-1; i++){
+            //다음날 오르면
+            if(prices[i+1]-prices[i] > 0) {
+                //이익을 더해준다.
+                profit += prices[i+1] - prices[i];
+            }
+        }
+        return profit;
+    }
+}
