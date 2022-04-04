f6f0e06cad2af958ad74d74a08de717f412660e6
gowoon <101627078+l22l22@users.noreply.github.com>
2022-04-04 16:51:01 +0900

    Update Best_Time_to_Buy_and_Sell_Stock_II.java

--- array/Best_Time_to_Buy_and_Sell_Stock_II.java
+++ array/Best_Time_to_Buy_and_Sell_Stock_II.java
@@ -11,4 +11,24 @@ class Solution {
         }
         return profit;
     }
+    /*
+        Max stock amount is only one.
+        If I want to get profit, I have to buy one after selling it.
+        
+        I can do 1. buy 2. sell 3. do nothing.
+        
+        If price keeps going up, your profit is same with total sum of profits.
+        ex) 1, 2, 3, 4, 5 = 5-1 = 2-1 + 3-2 + 4-3 + 5-4
+        
+        If price is going up and down, I have to sell before price going down.
+        This way is alway bigger than (max price - first buying price).
+        ex) 1,3,1,5 -> (3-1) + (5-1) > (5-1)
+        
+        If price keeps falling down I have to do nothing.
+        Also, If price doesnt change, I have to do nothing.
+        
+        So, All profit is same as sum of profit of each days going up prices.
+        
+        이익의 합은 다음날 값이 오른 경우의 오른 값들의 합과 같다. 
+    */
 }
